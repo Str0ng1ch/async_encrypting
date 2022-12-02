e87da6914b467a10a1b368270980b62fe100121f
Kuzmichev Artem <90923169+Str0ng1ch@users.noreply.github.com>
2022-12-02 09:06:55 +0300

    hot fix

--- client.py
+++ client.py
@@ -3,12 +3,21 @@ import pickle
 
 HOST = '127.0.0.1'
 PORT = 8080
+private_key = 157
+public_key = 151
+server_public_key = 197
 
 sock = socket.socket()
 sock.connect((HOST, PORT))
 
-p, g, a = 7, 5, 3
-A = g ** a % p
-sock.send(pickle.dumps((p, g, A)))
+partial_key = server_public_key ** private_key % public_key
+
+msg = sock.recv(1024)
+server_partial_key = pickle.loads(msg)
+
+sock.send(pickle.dumps(partial_key))
+
+full_key = server_partial_key ** private_key % public_key
+print('Полный ключ:', full_key)
 
 sock.close()
